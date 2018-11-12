56a01cacdead85b359d2318de476e518b7059be3
a-poodinemoghadam <44785814+a-poodinemoghadam@users.noreply.github.com>
2018-11-12 15:19:38 +0330

    Update exercise.py

--- hands-on exercise/932068002/exercise.py
+++ hands-on exercise/932068002/exercise.py
@@ -4,68 +4,62 @@
 import pyglet
 import time
 
+# kelas RainDropsAnimation sazande kelas method pyglet.window.Window ra farakhani mikonad.
 class RainDropsAnimation(pyglet.window.Window):
     def __init__(self, width = None, height = None):
-        pyglet.window.Window.__init__(self, width = width, height = height)
+        pyglet.window.Window.__init__(self,
+                                      width = width,
+                                      height = height)
         self.drawableObjects = []
         self.createDrawableObjects()
 
+    # ashya ya spriteha ra braye rasm ghatarat baran dar darone panjere pyglet ejad mikonad.
     def createDrawableObjects(self):
-        """
-        Create the objects (sprites) for drawing within the
-        pyglet Window.
-        """
         num_rows = 4
         num_columns = 1
         droplet = 'C:\\Users\\amir\\image\\droplet.png'
-        animation = self.setup_animation(droplet, num_rows, num_columns)
-
+        # manategh daron file tasvir ra baraye ejad framehaye animition shakhsi estefade mikonad.
+        animation = self.setup_animation(droplet,
+                                         num_rows,
+                                         num_columns)
+        # sprite baraye sheye moteharek ghatarat baran ejad shode ast.
         self.dropletSprite = pyglet.sprite.Sprite(animation)
         self.dropletSprite.position = (0,0)
 
         # Add these sprites to the list of drawables
         self.drawableObjects.append(self.dropletSprite)
 
+    # setup_animation sheye animition ra be vasileye manategh mokhtalef daron tasvir ejad mikonad.
     def setup_animation(self, img, num_rows, num_columns):
-        """
-        Create animation object using different regions of
-        a single image.
-        @param img: The image file path
-        @type img: string
-        @param num_rows: Number of rows in the image grid
-        @type num_rows: int
-        @param num_columns: Number of columns in the image grid
-        @type num_columns: int
-        """
+        # avalin nemone az tasvir dar khat zir ejad shode ast.
         base_image = pyglet.image.load(img)
-        animation_grid = pyglet.image.ImageGrid(base_image, num_rows, num_columns)
+
+        # yek nemone az ImageGrid ejad shode ast.
+        animation_grid = pyglet.image.ImageGrid(base_image,
+                                                num_rows,
+                                                num_columns)
         image_frames = []
 
         for i in range(num_rows*num_columns, 0, -1):
+            # mantaghe khas tasvir ra migirad va sepas anra ejad mikonad.
             frame = animation_grid[i-1]
-            animation_frame = pyglet.image.AnimationFrame(frame, 0.2)
+            animation_frame = pyglet.image.AnimationFrame(frame, 0.8)
+            # ghalebhaye animation mothark dar list image_frames zakhira shoda ast.
             image_frames.append(animation_frame)
-
+        # nemoney pyglet.image.Animation ba astafad az in list ejad meshavad.
         animation = pyglet.image.Animation(image_frames)
         return animation
-
+    # The overridden API method on_draw which is called when the Window. needs to be re-drawn.
     def on_draw(self):
-        """
-        The overridden API method on_draw which is called when the Window
-        needs to be re-drawn.
-        """
         self.clear()
         for d in self.drawableObjects:
             d.draw()
 
-
-# Create a Pyglet Window instance to show the animation.
+# yek nemone panjere az panjere pyglet baraye namayesh animation ejad mikonad.
 win = RainDropsAnimation()
 
-# Set window background color to white.
-r, g, b, alpha = 1, 1, 1, 1
+# rang background panjere ra ba rang firozeye set mikonad.
+r, g, b, alpha = 0, 1, 1, 1
 pyglet.gl.glClearColor(r, g, b, alpha)
 
 pyglet.app.run()
-
-
