84ce39f0c34e09080a5a0ea48583c794785106c5
niloofar8891 <44522603+niloofar8891@users.noreply.github.com>
2018-11-12 16:18:45 +0330

    Update exercise.py

--- hands-on exercise/931129504/exercise.py
+++ hands-on exercise/931129504/exercise.py
@@ -1,3 +1,19 @@
 #author: 931129504
 #exercise: Time for action – viewing an existing animation (page 98)
 
+import pyglet
+animation = pyglet.image.load_animation("C:\\Images\\SimpleAnimation.gif")
+animSprite = pyglet.sprite.Sprite(animation)
+w = animSprite.width
+h = animSprite.height
+win = pyglet.window.Window(width=w, height=h)
+r, g, b, alpha = 0.5, 0.5, 0.8, 1.
+pyglet.gl.glClearColor(r, g, b, alpha)
+
+@win.event
+def on_draw():
+    win.clear()
+    animSprite.draw()
+
+
+pyglet.app.run()
